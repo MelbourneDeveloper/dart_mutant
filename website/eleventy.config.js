824450197088eccf0840882c09e21b032ab010ff
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2025-12-15 21:42:06 +1100

    Fix cname

--- website/eleventy.config.js
+++ website/eleventy.config.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/fonts");
+  eleventyConfig.addPassthroughCopy("src/CNAME");
 
   // Watch CSS files for changes
   eleventyConfig.addWatchTarget("src/css/");
